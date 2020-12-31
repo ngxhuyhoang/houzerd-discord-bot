d18e9b65284abb7064cf2bee0c06e236f69f984d
Nguyen Huy Hoang <hoangnh@ahiho.com>
2021-01-01 04:01:21 +0700

    feat: remove build

--- build/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const Discord: any;
-declare const ytdl: any;
-declare const prefix = "hz!";
-declare const client: any;
-declare const queue: Map<any, any>;
-declare function execute(message: any, serverQueue: any): Promise<any>;
-declare function skip(message: any, serverQueue: any): any;
-declare function play(guild: any, song: any): void;
-//# sourceMappingURL=index.d.ts.map
