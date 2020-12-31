d24e28168519e989135e818a4804bf62f4b12250
Nguyen Huy Hoang <hoangnh@ahiho.com>
2021-01-01 03:59:52 +0700

    feat: create bot

---
+++ build/index.d.ts
@@ -0,0 +1,9 @@
+declare const Discord: any;
+declare const ytdl: any;
+declare const prefix = "hz!";
+declare const client: any;
+declare const queue: Map<any, any>;
+declare function execute(message: any, serverQueue: any): Promise<any>;
+declare function skip(message: any, serverQueue: any): any;
+declare function play(guild: any, song: any): void;
+//# sourceMappingURL=index.d.ts.map
